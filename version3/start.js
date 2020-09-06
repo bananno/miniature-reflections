9fec8f3779a8590c1336b4ef093c02b7e95a5ee6
Anna Bidstrup <annabidstrup20@gmail.com>
2020-09-05 22:21:49 -0700

    Fix weird newline characters

--- version3/start.js
+++ version3/start.js
@@ -1,25 +1,25 @@
 // INCLUDE ARRAYS
 
-	document.write('<script type="text/javascript" src="products.js"></script>');
+  document.write('<script type="text/javascript" src="products.js"></script>');
 
 // INCLUDE STYLE SHEET, ADD TITLE
 
 document.write('<link rel="stylesheet" type="text/css" href="../vendor/font.css">');
 
-	document.write('<link rel="stylesheet" type="text/css" href="style.css" /><title>Miniature Reflections</title><center>');
+  document.write('<link rel="stylesheet" type="text/css" href="style.css" /><title>Miniature Reflections</title><center>');
 
 // BEGIN OUTSIDE AND HEADER TABLES
 
-	document.write('<table class="outerTable"><td><table class="header"><td align=right>');
+  document.write('<table class="outerTable"><td><table class="header"><td align=right>');
 
 // MAIN HEADING WITH SUBHEADING TABLE
 
-	document.write('<table class="webLogo"><tr><td height=80><tr><td><font size=7>Miniature Reflections<td valign=top width=50>©<tr><td align=right><font size=5>Handcrafted Costumes for 18" Dolls<tr><td height=50></table>');
+  document.write('<table class="webLogo"><tr><td height=80><tr><td><font size=7>Miniature Reflections<td valign=top width=50>Â©<tr><td align=right><font size=5>Handcrafted Costumes for 18" Dolls<tr><td height=50></table>');
 
 // NAVIGATION LINKS, END HEADER TABLE
 
-	document.write('<table class="navigation" border=0 align=center height=40><td width=80 align=center><a href=index.html>Home<td>&#9830;<td width=100 align=center><a href=products.html>Products<td>&#9830;<td width=140 align=center><a href=order.html>How to Order<td>&#9830;<td width=120 align=center><a href=contact.html>Contact Us<td>&#9830;<td width=90  align=center><a href=gallery.html>Gallery</table></table>');
+  document.write('<table class="navigation" border=0 align=center height=40><td width=80 align=center><a href=index.html>Home<td>&#9830;<td width=100 align=center><a href=products.html>Products<td>&#9830;<td width=140 align=center><a href=order.html>How to Order<td>&#9830;<td width=120 align=center><a href=contact.html>Contact Us<td>&#9830;<td width=90  align=center><a href=gallery.html>Gallery</table></table>');
 
 // BEGIN MAIN BODY CONTENT TABLE
 
-	document.write('<table class="mainContent"><td>');
+  document.write('<table class="mainContent"><td>');
